c45e3c71e2d649041b4ba9c8838f77072f297bad
Ayaz <ayazzlockhat@gmail.com>
2025-04-12 01:01:06 -0400

    First creation of nextjs app for networth tracker app

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import styles from "../../styles/dashboard/Dashboard.module.css";
+
+
+export default function DashboardPage() {
+  return (
+    <main className={styles.wrapper}>
+      <h1 className={styles.heading}>📊 Dashboard is live!</h1>
+    </main>
+  )
+}
